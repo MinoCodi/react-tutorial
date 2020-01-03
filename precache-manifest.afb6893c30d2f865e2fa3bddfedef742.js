0c343492f6ad2007d4704753db682a574c6afb23
MinoCodi <48563099+MinoCodi@users.noreply.github.com>
2020-01-03 14:44:09 +0000

    Add files via upload

---
+++ precache-manifest.afb6893c30d2f865e2fa3bddfedef742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3317b36c05f6ba83f375e8bcfe30ea",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "d8978406ca8ad00339eb",
+    "url": "/react-tutorial/static/css/main.912e857b.chunk.css"
+  },
+  {
+    "revision": "586bd8ceafe4fd8d8baf",
+    "url": "/react-tutorial/static/js/2.1c92a7e0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tutorial/static/js/2.1c92a7e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8978406ca8ad00339eb",
+    "url": "/react-tutorial/static/js/main.ac01e19d.chunk.js"
+  },
+  {
+    "revision": "487c6f96f729a325c0db",
+    "url": "/react-tutorial/static/js/runtime-main.6bddf0c2.js"
+  }
+]);
